d31af26181317d646f23ca1eb71ff385ba78673d
Refaat Mohammad Alamgir <refaat.alamgir@gmail.com>
2024-08-14 04:25:41 +0600

    the worst search & UI in the world

--- scripts/load.js
+++ scripts/load.js
@@ -1,1 +1,56 @@
-fetch('https://raw.githubusercontent.com/refaat31/unifo/main/data/final/r1_cleaned_final.json').then(x => x.text()).then(y=> document.getElementById('display').innerText=y)
+let combined;
+let uni = [];
+fetch(
+  "https://raw.githubusercontent.com/refaat31/unifo/main/data/final/combined_final.json"
+)
+  .then((x) => x.text())
+  .then((y) => {
+    combined = JSON.parse(y);
+    console.log(combined);
+    for (let i = 0; i < combined.length; i++) {
+      uni.push(combined[i]["Institution"]);
+    }
+    console.log(uni);
+  });
+
+function myFunction() {
+  $("#mySearch").autocomplete({
+    source: function (request, response) {
+      var matcher = new RegExp(
+        "^" + $.ui.autocomplete.escapeRegex(request.term),
+        "i"
+      );
+      response(
+        $.grep(uni, function (item) {
+          return matcher.test(item);
+        })
+      );
+    },
+  });
+}
+
+function display(e) {
+  if (true) {
+    e.preventDefault();
+
+    let search = document.getElementById("mySearch").value;
+    for (let i = 0; i < combined.length; i++) {
+      if (search === combined[i]["Institution"]) {
+        document.getElementById("display").innerText = JSON.stringify(
+          combined[i]
+        );
+        // {
+        //   "Institution": combined[i]['Institution'],
+        //   "Control": combined[i]['Control'],
+        //   "City": combined[i]['City'],
+        //   "State": combined[i]['State'],
+        //   "Category": combined[i]['Category']
+        // };
+        break;
+      }
+    }
+  }
+
+  // e.preventDefault()
+  // alert(`${cal}`)
+}
